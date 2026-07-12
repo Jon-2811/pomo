a639a6185dfca36e6669dad4e626a458ce8f4ed4
Jon-2811 <aiuuuuueeeee@gmail.com>
2026-07-12 18:05:26 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,9 @@
-/*
-  複数端末同期を使う場合だけ、Firebaseコンソールに表示される設定値へ置き換えてください。
-  この設定値自体は秘密鍵ではありません。データ保護は firestore.rules で行います。
-*/
 window.FOCUS_GLASS_FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyDc4jbJHMAp9nQ-5_dfLcEhzh3zxaUQAXs",
+  authDomain: "pomo-ac014.firebaseapp.com",
+  projectId: "pomo-ac014",
+  storageBucket: "pomo-ac014.firebasestorage.app",
+  messagingSenderId: "415444443389",
+  appId: "1:415444443389:web:9c4e3b599a941d3360de94",
+  measurementId: "G-88DP270JQ4"
 };
